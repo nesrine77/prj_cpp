847684e4ac809f359c8525985ac06c161297639d
nesrine77 <130911133+nesrine77@users.noreply.github.com>
2023-11-22 21:28:04 +0100

    Add files via upload
    second commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "compte.h"
+#include "ce.h"
+using namespace std;
+
+int main()
+{
+    compte c;
+    ce c1("12",200,10);
+    c1.afficher(12);
+	
+    return 0;
+
+}
